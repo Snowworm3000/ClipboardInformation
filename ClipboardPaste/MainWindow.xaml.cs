0a991e8fd4b9c66e78927fc79fb61bdd596b8bf4
Ethan Burkinshaw <61204146+Snowworm3000@users.noreply.github.com>
2022-01-04 17:45:44 +0000

    Add project files.

---
+++ ClipboardPaste/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClipboardPaste
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonPaste_Click(object sender, RoutedEventArgs e)
+        {
+            IDataObject data = Clipboard.GetDataObject(); //Gets all clipboard data
+            string[] formats = data.GetFormats(); //Gets all available clipboard formats
+            
+            lstContentType.Items.Clear(); //Clears the content type list before populating it 
+            List<object> clipboardContents = new List<object>();
+            for (int i = 0; i < formats.Length; i++) 
+            {
+                var content = data.GetData(formats[i]);
+                clipboardContents.Add(content);
+                lstContentType.Items.Add(formats[i]);
+                
+            }
+
+            
+
+            lstContent.Items.Clear(); //Clears the content list before populating it 
+            foreach (var content in clipboardContents) //Adds clipboard content to the list
+            {
+                lstContent.Items.Add(content);
+            }
+            
+            printList(clipboardContents); // Also prints the clipboard's contents for debugging purposes
+        }
+
+        private void printList(List<object> list)
+        {
+            if(list != null) { 
+                foreach (var i in list)
+                {
+                    Debug.WriteLine(i);
+                }
+            }
+        }
+    }
+}
